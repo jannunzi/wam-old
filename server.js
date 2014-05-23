2a66b0efca5b98a626eb633783725eebe56246d5
jannunzi <jannunzi@gmail.com>
2014-05-23 12:22:23 -0400

    WAM to Openshift

--- server.js
+++ server.js
@@ -1,49 +1,18 @@
-var mongoose	= require("mongoose");
 var express	= require('express');
-var server		= express();
-var port		= process.env.PORT || 8080;
-mongoose.connect("mongodb://localhost/wam");
+var app = express();
+var port		= process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var ipaddress	= process.env.OPENSHIFT_NODEJS_IP;
 
-var ApplicationSchema = mongoose.Schema({
-	name: String,
-	create: {type: Date, default: Date.now},
-	price: {type: Number, default: 0.0}
+app.configure(function() {
+	app.use(express.static(__dirname + '/public')); 		// set the static files location /public/img will be /img for users
+	app.use(express.logger('dev')); 						// log every request to the console
+	app.use(express.bodyParser()); 							// pull information from html in POST
+	app.use(express.methodOverride()); 						// simulate DELETE and PUT
 });
 
-var Application = mongoose.model("Application", ApplicationSchema);
-
-server.configure(function() {
-	server.use(express.static(__dirname + '/public'));
-	server.use(express.bodyParser());
-});
-
-server.listen(port);
-
-server.get("/api/hello/:username", function(req, res) {
+app.get("/api/hello/:username", function(req, res) {
 	var username = req.params.username;
 	res.json({message: "Hello " + username});
 });
 
-server.get("/api/application", function(req, res) {
-	Application.find(function(err, applications){
-		res.json(applications);
-	});
-});
-
-server.post("/api/application", function(req, res) {
-	Application.create(req.body, function(err, application){
-		Application.find(function(err, applications){
-			res.json(applications);
-		});
-	});
-});
-
-server.delete("/api/application/:id", function(req, res) {
-	Application.findById(req.params.id, function(err, application) {
-		application.remove(function(err, app){
-			Application.find(function(err, applications){
-				res.json(applications);
-			});
-		});
-	});
-});
+app.listen(port, ipaddress);
